e586f396524ade2d1388a9698d53185d01a3ab38
shahzad0192 <shahzadjani9065@gmail.com>
2025-09-24 17:58:49 -0700

    update

---
+++ config.py
@@ -0,0 +1,23 @@
+
+
+URLS_LIKE = {"IND":"https://client.ind.freefiremobile.com/LikeProfile",
+            "BR":"https://client.us.freefiremobile.com/LikeProfile",
+            "US":"https://client.us.freefiremobile.com/LikeProfile",
+            "SAC":"https://client.us.freefiremobile.com/LikeProfile",
+            "PK":"https://client.us.freefiremobile.com/LikeProfile"}
+
+
+URLS_INFO = {"IND":"https://client.ind.freefiremobile.com/GetPlayerPersonalShow",
+            "BR":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+            "US":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+            "SAC":"https://client.us.freefiremobile.com/GetPlayerPersonalShow",
+            "PK":"https://client.us.freefiremobile.com/GetPlayerPersonalShow"}
+            
+
+
+
+FILES = {"IND":"token_ind.json",
+             "BR":"token_br.json",
+             "US":"token_br.json",
+             "SAC":"token_br.json",
+             "PK":"token_pk.json"}
